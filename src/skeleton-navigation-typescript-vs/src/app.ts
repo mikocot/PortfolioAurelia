bc1b43a85710bd44bacea1fdea7045aa0aa8d60b
Mikolaj K <kocot@live.com>
2016-04-17 19:45:59 +0200

    additional dependencies, cleaning of skeleton content

--- src/skeleton-navigation-typescript-vs/src/app.ts
+++ src/skeleton-navigation-typescript-vs/src/app.ts
@@ -4,11 +4,10 @@ export class App {
   router: Router;
   
   configureRouter(config: RouterConfiguration, router: Router) {
-    config.title = 'Aurelia';
+    config.title = 'Mikolaj Kocot Portfolio';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
+      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Start' },
+      { route: 'gallery',         name: 'gallery',        moduleId: 'gallery',       nav: true, title: 'Gallery' },
     ]);
 
     this.router = router;
